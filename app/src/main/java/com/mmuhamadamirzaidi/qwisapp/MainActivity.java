a756a6141be79ef6f2c2b892b07fcbd553e0d650
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-07-02 01:19:54 +0800

    Add Sign In Page

--- app/src/main/java/com/mmuhamadamirzaidi/qwisapp/MainActivity.java
+++ app/src/main/java/com/mmuhamadamirzaidi/qwisapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.mmuhamadamirzaidi.qwisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
