12919a498146fd0deae2409c60c65fc2a059a010
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-07-01 18:12:57 +0800

    Add Splash Screen

--- app/src/main/java/com/mmuhamadamirzaidi/qwisapp/SplashActivity.java
+++ app/src/main/java/com/mmuhamadamirzaidi/qwisapp/SplashActivity.java
@@ -1,13 +1,36 @@
 package com.mmuhamadamirzaidi.qwisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView SplashLogo;
+    private int SPLASH_TIME_OUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        SplashLogo = (ImageView)findViewById(R.id.splash_logo_startup);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splash = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(splash);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.splashanimation);
+        SplashLogo.startAnimation(myanim);
     }
 }
