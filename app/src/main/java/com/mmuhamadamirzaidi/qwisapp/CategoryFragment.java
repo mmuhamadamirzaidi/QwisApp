8ddcad256b5eed03dd035a224e5c8b6dee18542e
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-07-02 20:57:25 +0800

    Add Category Fragment

---
+++ app/src/main/java/com/mmuhamadamirzaidi/qwisapp/CategoryFragment.java
@@ -0,0 +1,32 @@
+package com.mmuhamadamirzaidi.qwisapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class CategoryFragment extends Fragment {
+
+    View myFragment;
+
+    public static  CategoryFragment newInstance(){
+        CategoryFragment categoryFragment = new CategoryFragment();
+        return categoryFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        myFragment = inflater.inflate(R.layout.fragment_category, container, false);
+        return myFragment;
+    }
+}
