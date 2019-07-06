227fc967441afb5efd012b6af923b2c5088d27f3
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-07-06 13:13:12 +0800

    Finish Layout Play Game

---
+++ app/src/main/java/com/mmuhamadamirzaidi/qwisapp/StartGameActivity.java
@@ -0,0 +1,92 @@
+package com.mmuhamadamirzaidi.qwisapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.transition.Fade;
+import android.transition.TransitionInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class StartGameActivity extends AppCompatActivity {
+
+    TextView CategoryTitle, CategorySubtitle;
+
+    Button ButtonCategoryPlayQuiz;
+    Animation AnimationOne, AnimationTwo, AnimationThree;
+    ImageView CategoryImageIcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_game);
+
+        CategoryImageIcon = findViewById(R.id.categoryimageicon);
+        CategoryTitle = findViewById(R.id.categorytitle);
+        CategorySubtitle = findViewById(R.id.categorysubtitle);
+        ButtonCategoryPlayQuiz = findViewById(R.id.buttoncategoryplayquiz);
+
+        if (Build.VERSION.SDK_INT>=21) {
+            setupWindowAnimations();
+        }
+
+        //Load animations
+        AnimationOne = AnimationUtils.loadAnimation(this, R.anim.animation_dashboard_one);
+        AnimationTwo = AnimationUtils.loadAnimation(this, R.anim.animation_dashboard_two);
+        AnimationThree = AnimationUtils.loadAnimation(this, R.anim.animation_dashboard_three);
+
+        //Pass animations
+        CategoryImageIcon.startAnimation(AnimationOne);
+
+        CategoryTitle.startAnimation(AnimationTwo);
+        CategorySubtitle.startAnimation(AnimationTwo);
+
+        ButtonCategoryPlayQuiz.startAnimation(AnimationThree);
+
+        ButtonCategoryPlayQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(StartGameActivity.this, "Play Game!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @TargetApi(21)
+    private void setupWindowAnimations(){
+        try {
+            Fade fade = (Fade) TransitionInflater.from(this).inflateTransition(R.transition.activity_fade);
+            getWindow().setExitTransition(fade);
+        } catch (NoClassDefFoundError e){
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        super.onPause();
+    }
+
+    private RoundedBitmapDrawable getRoundedDrawable(String filename){
+        try {
+            RoundedBitmapDrawable dr = RoundedBitmapDrawableFactory.create(getResources(), getAssets().open(filename));
+            dr.setCornerRadius(500);
+            return dr;
+        }
+        catch (IOException e){
+//            Log.d("imageHandling", e.toString());
+        }
+        return null;
+    }
+}
