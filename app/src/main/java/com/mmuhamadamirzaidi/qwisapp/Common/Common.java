8571658b1d555b45b19ca629990cf74774fcfef3
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-07-06 20:31:17 +0800

    Declair ArrayList For Question

--- app/src/main/java/com/mmuhamadamirzaidi/qwisapp/Common/Common.java
+++ app/src/main/java/com/mmuhamadamirzaidi/qwisapp/Common/Common.java
@@ -1,6 +1,12 @@
 package com.mmuhamadamirzaidi.qwisapp.Common;
 
+import com.mmuhamadamirzaidi.qwisapp.Model.Questions;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Common {
 
     public static String categoryId;
+    public static List<Questions> ListQuestion = new ArrayList<>();
 }
