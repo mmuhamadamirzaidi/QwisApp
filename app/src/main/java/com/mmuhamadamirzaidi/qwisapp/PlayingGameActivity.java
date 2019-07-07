bab2260f0dde78eebad959c3d539f1b594780652
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-07-07 15:55:13 +0800

    Add Function For Game Play

---
+++ app/src/main/java/com/mmuhamadamirzaidi/qwisapp/PlayingGameActivity.java
@@ -0,0 +1,14 @@
+package com.mmuhamadamirzaidi.qwisapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayingGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playing_game);
+    }
+}
