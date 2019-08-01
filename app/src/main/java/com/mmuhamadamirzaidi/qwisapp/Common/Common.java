8a3ce850e6c96687b983454c8a653a6b2638d777
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-08-01 18:06:52 +0800

    Refactor Coding

--- app/src/main/java/com/mmuhamadamirzaidi/qwisapp/Common/Common.java
+++ app/src/main/java/com/mmuhamadamirzaidi/qwisapp/Common/Common.java
@@ -7,12 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Common {
-
-<<<<<<< HEAD
-    public static String categoryId;
-=======
     public static String categoryId, categoryName;
->>>>>>> master
     public static User currentUser;
     public static List<Questions> ListQuestion = new ArrayList<>();
 }
